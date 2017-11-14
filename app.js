a4e5751c91229d4520fe93c344a241533828167b
Blake Harris <harrisblake85@gmail.com>
2017-11-14 14:09:59 -0700

    added win conditions

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 console.log($);
 $(() => {
   //start window onload
+  let thisgame = null;
   let $container = $(".container");
   let $sky = $("<div>").addClass("sky");
   $container.append($sky);
   let $grass = $("<img>").addClass("grass").attr("src","images/grass.jpg");
+  $container.append($grass);
   let $body = $("body");
   let players=[];
   let money=[];
-
-
 
   class Player {
     constructor() {
@@ -17,7 +17,9 @@ $(() => {
       this.src2 = "images/tophat/tophat_jump.png";
       this.src3 = "images/tophat/tophat_fall.png";
       this.score = 0;
+      this.won = false;
       this.ai=false;
+      this.parachute=false;
       this.jumping = false;
       this.falling = false;
       this.offgrass = false;
@@ -25,7 +27,6 @@ $(() => {
       this.offgrassright = false;
       this.wpress=false;
       this.apress=false;
-      this.spress=false;
       this.dpress=false;
       this.choice=2;
       this.x=0;
@@ -35,6 +36,19 @@ $(() => {
       //begin game interval
       // game interval determines what moves a player/ai takes
       this.game =setInterval(() => {
+        //determine whether or not player is parachuting
+        if (this.$player.height()>=596) {
+          this.won=true;
+          if (this.parachute) {
+
+          }
+          else {
+            this.$player.attr("src",this.src1);
+            this.parachute=true;
+          }
+
+        }
+        //end parachute
         //ai section, only active if this.ai=true
         if (this.ai) {
           //ai self preservation, ai still may fall due to .stop()
@@ -56,7 +70,7 @@ $(() => {
             this.$player.stop();
             this.choices=["right","left","jump"];
             this.choice = Math.floor(Math.random()*3);
-            console.log(this.choices[this.choice]);
+            // console.log(this.choices[this.choice]);
             //ai jump
             if (this.choices[this.choice]=="jump") {
               if (this.wpress) {
@@ -97,7 +111,7 @@ $(() => {
         //end ai
         //wpress
         if (this.wpress) {
-          if (this.falling) {
+          if (this.falling||this.parachute) {
           }
           else {
             this.jumping = true;
@@ -107,7 +121,7 @@ $(() => {
         //end wpress
         //grass gravity player falls if they jump off the grass
         //fall left
-        console.log($grass.position().left);
+        // console.log($grass.position().left);
 
         if ($grass.position().left-this.$player.width()>this.$player.position().left) {
           this.playerdown();
@@ -149,7 +163,14 @@ $(() => {
             this.falling=false;
           }
         }
+
         //jump gravity end
+        //if player falls out of bounds
+        if (this.$player.position().top>1100)
+        {
+          this.killplayer();
+        }
+        //end out of bounds
         //left and right
         if (this.apress) {
           this.playerleft();
@@ -184,6 +205,12 @@ $(() => {
         'left' : "+=3px" //movesright
       },0);
     }
+    killplayer(){
+      this.wasai = this.ai;
+      this.$player.remove();
+      clearInterval(this.game);
+      thisgame.endgame(this.wasai,this.won);
+    }
 
   }
   // end player class
@@ -200,112 +227,186 @@ $(() => {
   }
   //end ai class
 
-  class Money extends AI{
+  class Money{
     constructor() {
-      super();
       this.src3="images/money.png";
-      this.$player.attr("src",this.src3);
+      this.$money = $("<img>").addClass("player");
+      this.$money.attr("src",this.src3);
       //start money over container, within bounds of grass
-      this.px =Math.floor(Math.random()*901);
+      this.px =Math.floor(Math.random()*897)+3;
       this.py = -110;
-      this.$player.css( { left: this.px, top: this.py } ) ;
-      this.$player.width(80);
-      this.$player.height(40);
-
+      this.$money.css( { left: this.px, top: this.py } ) ;
+      this.$money.width(80);
+      this.$money.height(40);
+      this.fall=setInterval(() => {
+        if (this.$money.position().top+this.$money.height()==$grass.position().top) {
+          //if money is on the grass do nothing
+        }
+        else {
+          //else have the money fall down
+          this.moneydown();
+        }
+      },0)
     }
-    // playerup()
-    // {
-    //   //money doesnt jump up
-    // }
+    moneydown()
+    {
+      this.$money.animate({
+        'top' : "+=1px" //movesdown
+      },0);
+    }
   }
-  const makedollar = () => {
-      let tempdollar = new Money();
-      $container.prepend(tempdollar.$player);
-      money.push(tempdollar);
 
-      // tempdollar.$player.css({position:"absolute"});
-  }
+  //add the grass to the container
+
 
   class Game {
     constructor() {
+      //add a human player and two ai to players array
+        this.human = new Player();
+        this.ai = new AI();
+        this.ai2 = new AI();
+        players = [this.human,this.ai,this.ai2];
+        //add players in this for loop
+        for (let i = 0; i < players.length; i++) {
+          $container.append(players[i].$player);
+        }
+        //end add players for loop
+      this.gameover=false;
       this.rainmoney = setInterval(() => {
-        makedollar();
-      },4000);
-      this.collect = setInterval(() => {
+        if (money.length<6) {
+          this.makedollar();
+        }
 
+      },1000);
+      //collect, check if any player is overlapping money
+      this.collect = setInterval(() => {
+        // for money array
         for (let i = 0; i < money.length; i++) {
-          this.mleft = money[i].$player.position().left;
-          this.mright = money[i].$player.position().left+this.$player.width();
-          this.mtop = money[i].$player.position().top;
-          this.mbottom = money[i].$player.position().top+this.$player.height();
-          for (let i = 0; i < money.length; i++) {
-            this.pleft = player[i].$player.position().left;
-            this.pright = player[i].$player.position().left+this.$player.width();
-            this.ptop = player[i].$player.position().top;
-            this.pbottom = player[i].$player.position().top+this.$player.height();
-            if (true) {
+          this.mleft = money[i].$money.position().left;
+          this.mright = money[i].$money.position().left+money[i].$money.width();
+          this.mtop = money[i].$money.position().top;
+          this.mbottom = money[i].$money.position().top+money[i].$money.height();
+          //for players array
+          for (let j = 0; j < players.length; j++) {
+            this.pleft = players[j].$player.position().left;
+            this.pright = players[j].$player.position().left+players[j].$player.width();
+            this.ptop = players[j].$player.position().top;
+            this.pbottom = players[j].$player.position().top+players[j].$player.height();
+            this.overlap = !(this.mright < this.pleft ||
+                this.mleft > this.pright ||
+                this.mbottom < this.ptop ||
+                this.mtop > this.pbottom);
+            if (this.overlap) {
+              //
+              this.grow(j);
+              //
+              //if overlapping remove money
+              clearInterval(money[i].fall);
+              money[i].$money.remove();
+              money.splice(i, 1);
+              //end remove money
+
+              //give player a boosts if they collect
+              if (players[j].length>596) {
+                players[j].won=true;
+              }
+              else {
+                for (let i = 0; i < 10; i++) {
+                  players[j].playerup();
+                }
+              }
+              //end boost
 
             }
+            //overlap if
           }
+          //end for players array
         }
-        if (false) {
-          console.log("not yet");
+        //end for money array
+      },0)
+      //end collect
+    }
+    makedollar(){
+        money.push(new Money());
+        $container.prepend(money[money.length-1].$money);
+        //this function adds money to the game
+    }
+    grow(j){
+      //if player collected money have them grow and score
+      players[j].score ++;
+      players[j].$player.width( players[j].$player.width()*1.1);
+      players[j].$player.width(Math.floor( players[j].$player.width()));
+      players[j].$player.height( players[j].$player.height()*1.1);
+      players[j].$player.height(Math.floor( players[j].$player.height()));
+      //end grow player
+    }
+    endgame(wasai,won)
+    {
+      clearInterval(this.collect);
+      clearInterval(this.rainmoney);
+
+      for (let i = 0; i < players.length; i++) {
+        clearInterval(players[i].game);
+        players[i].$player.remove();
+      }
+      players.length=0;
+
+      for (let i = 0; i < money.length; i++) {
+        clearInterval(money[i].fall);
+        money[i].$money.remove();
+      }
+      money.length=0;
+
+      if (this.gameover) {
+        $sky.text("game already over");
+        console.log("game already over");
+      }
+      else {
+        if (wasai) {
+          $sky.text("player lost!");
+          console.log("player lost!");
         }
-      },20)
+        else if (won) {
+          $sky.text("player won");
+          console.log("player won");
+        }
+        else {
+          $sky.text("player lost by falling");
+          console.log("player lost by falling");
+        }
+      }
+
     }
   }
 
   thisgame = new Game();
-  human = new Player();
-  ai = new AI();
-  ai2 = new AI();
-  players = [human,ai,ai2];
-  for (let i = 0; i < players.length; i++) {
-    $container.append(players[i].$player);
-  }
-  $container.append($grass);
 
-
-
-  human.$player.on("click",() => {
-    console.log(human.$player.height());
-    human.$player.width( human.$player.width()*1.02);
-    human.$player.width(Math.floor( human.$player.width()));
-    human.$player.height( human.$player.height()*1.01);
-    human.$player.height(Math.floor( human.$player.height()));
-  })
-
+//event handlers for human player movement
   $body.keypress(() => {
-    human.$player.stop();
+    players[0].$player.stop();
 
     if (event.key=="w") {
-      human.wpress=true;
+      players[0].wpress=true;
     }
     if (event.key=="a") {
-      human.apress=true;
-    }
-    if (event.key=="s") {
-      human.spress=true;
+      players[0].apress=true;
     }
     if (event.key=="d") {
-      human.dpress=true;
+      players[0].dpress=true;
     }
   });
 
   $body.keyup(() => {
-    human.$player.stop();
+    players[0].$player.stop();
 
     if (event.key=="w") {
-      human.wpress=false;
+      players[0].wpress=false;
     }
     if (event.key=="a") {
-      human.apress=false;
-    }
-    if (event.key=="s") {
-      human.spress=false;
+      players[0].apress=false;
     }
     if (event.key=="d") {
-      human.dpress=false;
+      players[0].dpress=false;
     }
   });
 
