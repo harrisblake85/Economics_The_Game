edbb501393032927769f375c2451eee3db9e14f5
Blake Harris <harrisblake85@gmail.com>
2017-11-16 07:54:01 -0700

    added instructions

--- app.js
+++ app.js
@@ -485,7 +485,27 @@ $(() => {
       thisgame=new Socialism();
       $sky.text("Socialism The Game");
 
-    })
+    });
+    const $openBtn = $('#openModal');
+    const $modal = $('#modal');
+    const $closeBtn = $('#close');
+
+    //Event Handlers
+    const openModal = () => {
+      $modal.css('display', 'block');
+    }
+
+    const closeModal = () => {
+      $modal.css('display', 'none');
+    }
+
+    //Event Listeners
+    $openBtn.on('click', openModal);
+
+    $closeBtn.on('click', closeModal);
+
+    // setTimeout(openModal,1000);
+    // setTimeout(closeModal,2000);
 
     //event handlers for human player movement
 
